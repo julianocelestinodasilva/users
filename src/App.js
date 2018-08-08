cb7f5307be1dd9cc4fafde5e04469c382f7e5494
juliano <jcelestino@raiadrogasil.com.br>
2018-08-08 19:47:23 -0300

    Realizando Get na API de Users

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import './App.css';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://172.19.0.2:8080/users")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            users: result.items
+          });
+        },
+        (error) => {
+          console.log('Erro ao consultar os Usuarios --> ' + error.message);
+          this.setState({
+            isLoaded: true,
+          });
+        }
+      )
+  }
   
   render() {
 
@@ -17,28 +43,32 @@ class App extends Component {
     ];
 
     const fields = [
+      { name: 'username', displayName: "Username", inputFilterable: true, sortable: true },
+      { name: 'password', displayName: "Password", inputFilterable: false, sortable: false },
+      { name: 'registerDate', displayName: "Register Date", inputFilterable: false, sortable: false },
       { name: 'name', displayName: "Name", inputFilterable: true, sortable: true },
-      { name: 'age', displayName: "Age", inputFilterable: true, exactFilterable: true, sortable: true },
-      { name: 'job', displayName: "Occupation", inputFilterable: true, exactFilterable: true, sortable: true }
+      { name: 'surname', displayName: "Surname", inputFilterable: false, sortable: false },
+      { name: 'email', displayName: "Email", inputFilterable: true, sortable: true },
+      { name: 'fone', displayName: "Fone", inputFilterable: false, sortable: false },
+      { name: 'enabled', displayName: "Enabled", inputFilterable: false, sortable: false },
     ];
 
     return (
       <div className="App">
         <header className="App-header">
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
 
-        <div>
-          <FilterableTable
-            initialSort="name"
-            data={data}
-            fields={fields}
-            noRecordsMessage="There are no people to display"
-            noFilteredRecordsMessage="No people match your filters!"
-          />
-        </div>  
+        <p>
+          <div>
+           <FilterableTable
+              initialSort="name"
+              data={this.state.users}
+              fields={fields}
+              noRecordsMessage="There are no people to display"
+              noFilteredRecordsMessage="No people match your filters!"
+            />
+          </div>  
+        </p>  
 
       </div>
     );
